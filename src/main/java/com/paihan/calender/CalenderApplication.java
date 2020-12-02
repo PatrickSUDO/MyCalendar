441f38de0ec39afccdbd800b64b196b66e017ff9
PatrickSUDO <patricksuph@gmail.com>
2020-12-02 22:20:35 +0000

    first commit

---
+++ src/main/java/com/paihan/calender/CalenderApplication.java
@@ -0,0 +1,13 @@
+package com.paihan.calender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderApplication {
+
+    public static void main(String[] args) throws Throwable{
+        SpringApplication.run(CalenderApplication.class, args);
+    }
+
+}
